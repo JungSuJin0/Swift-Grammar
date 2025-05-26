60e20c39438722206451e5bde510471e02f1273e
JUNG SU JIN <114715283+JungSuJin0@users.noreply.github.com>
2025-05-26 21:45:31 +0900

    Update Contents.swift

--- day_10.playground/Contents.swift
+++ day_10.playground/Contents.swift
@@ -1,28 +1,29 @@
 import UIKit
+//클로저
 
-//String을 반환하는 클로저
+
+//형태: { (매개변수) -> 반환형 in 실행코드 }
+//값을 반환하는 클로저
 let myName: String = {
-    //myName 으로 들어감
     return "수진"
 } ()
 print(myName)
 
 
-//클로저 정의
+//매개변수를 받고 값을 반환하는 클로저
+//문자열 이름 받아서 문자열을 반환하는 클로저
 //let myRealName = { name: String) -> String in
 //    return "개발하는 \(name)"
 //}
-//String으로 받아서 String으로 보내는
 
 let myRealName: (String) -> String = { (name: String) -> String in
     return "개발하는 \(name)"
 }
+myRealName("수진")
 
-myRealName("Jung Su 조건Jin")
 
-//String으로 받아서 내뱉는게 딱히 없다 할 때 Void를 써준다.
+//값을 받고 아무것도 반환하지 않는 클로저 -> 반환형이 Void, 즉 ()
 let myRealNameLogic: (String) -> Void = {(name: String) in
     print("개발하는 \(name)")
 }
-
-myRealNameLogic("Hi")
+myRealNameLogic("수진 안녕")
