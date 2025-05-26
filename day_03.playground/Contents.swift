58a67704e02d3888d64f5c7b80fb9a0a0ae4682f
JUNG SU JIN <114715283+JungSuJin0@users.noreply.github.com>
2025-05-26 17:35:27 +0900

    Update Contents.swift

--- day_03.playground/Contents.swift
+++ day_03.playground/Contents.swift
@@ -1,31 +1,32 @@
 import UIKit
 //enum 케이스 나누기
 
-// 타입을 나누다(=케이스만 나누다)
+
+//case 별로 나누기
 enum School {
     case elementry
     case middle
     case high
-//이렇게 표현도 가능 -> case elementry, middle, high
-}
+  //case elementry, middle, high  //같은 의미
 
 let yourSchool = School.elementry
 print("yourSchool: \(yourSchool)")
-//이렇게 표현도 가능 -> print("yourSchool: " yourSchool)
-
 print("-----------------------")
-//값을 주다
+
+    
+//enum에 값 부여
 enum Grade: Int {
     case first = 1
     case second = 2
 }
 
-//값에 접근
 let yourGrade = Grade.second
-//print("yourGrade: \(yourGrade)")
-print("yourGrade: \(yourGrade.rawValue)")
-
+//let yourGrade = Grade.second.rawValue  //print문 생략가능, 2
+//print("yourGrade: \(yourGrade)")  //second
+print("yourGrade: \(yourGrade.rawValue)")  //2
 print("-----------------------")
+
+    
 enum SchoolDetail {
     //케이스를 정해줄때 데이터를 지정
     case elementary(name: String)
@@ -33,6 +34,8 @@ enum SchoolDetail {
     case high(name: String)
     
     //함수 지정
+    //enum 값 안에 저장된 이름(name)을 꺼내는 역할
+    //let 없을시 name이 상수인지 변수인지 알 수가 없어 에러발생 → 그래서 반드시 let name, 또는 var name처럼 꺼낸 값이 상수인지 변수인지 명확하게 표시
     func name() -> String {
         switch self {
         case.elementary(let name):
@@ -46,7 +49,7 @@ enum SchoolDetail {
 }
 
 let yourMiddleSchoolName = SchoolDetail.middle(name: "수진중학교")
-print("yourMiddleSchoolName: \(yourMiddleSchoolName)")
-
+print("yourMiddleSchoolName: \(yourMiddleSchoolName)")  //yourMiddleSchoolName: middle(name: "수진중학교")
 print("-----------------------")
-print("yourMiddleSchoolName: \(yourMiddleSchoolName.name())")
+
+print("yourMiddleSchoolName: \(yourMiddleSchoolName.name())")  //yourMiddleSchoolName: 수진중학교
