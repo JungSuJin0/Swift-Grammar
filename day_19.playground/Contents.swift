622a170abc9354bb0ffa5a8c6e7df3de9e8528b4
JUNG SU JIN <114715283+JungSuJin0@users.noreply.github.com>
2025-05-28 15:11:13 +0900

    Update Contents.swift

--- day_19.playground/Contents.swift
+++ day_19.playground/Contents.swift
@@ -1,6 +1,9 @@
 import UIKit
+//Set
+//배열처럼 중복으로 값을 넣을 수 없음, 아무리 넣어도 고유한 값들만 남음
 
-//var myNumberSet
+
+//var myNumberSet: Set<Int> = Set<Int>() 
 var myNumberSet = Set<Int>()
 
 myNumberSet.insert(1)
@@ -9,24 +12,24 @@ myNumberSet.insert(2)
 myNumberSet.insert(3)
 myNumberSet.insert(3)
 
-myNumberSet.count
-myNumberSet
+print("myNumberSet.count: \(myNumberSet.count)")
 
-//
+
+//배열과 다르게 순서가 정해져 있지 않음
+//매번 출력되는 값들의 순서가 다름
 for i in myNumberSet {
     print("aNumber:", i)
 }
 
+
+//그 외에도 콜렉션 (배열, 셋, 딕셔너리, 튜플)등이 가지고 있는 기본 메소드들을 제공
 var myFriends: Set<String> = ["철수", "영희", "수지"]
-var myBestFriends: Set<String> = ["철수", "영희", "수지"]
+if myFriends.contains("철수") {
+    print("내 친구들중에 철수가 있다")
+}
 
-myFriends.contains("수지")
-myFriends.contains("철수")
-
-//item 에
+//수지의 인덱스 지우기
 if let indexToRemove = myFriends.firstIndex(of: "수지") {
-    // 수지를 지운다.
-    print("indexToRemove: ", indexToRemove)
     myFriends.remove(at: indexToRemove)
 }
 
